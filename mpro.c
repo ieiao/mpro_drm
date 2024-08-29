f5acfb3fb7146fcf6885fb72d136963fed2600c5
ieiao <ieiao@outlook.com>
2024-08-29 13:47:18 +0800

    Simplified method for obtaining screen parameters
    Maybe will break compatibility

--- mpro.c
+++ mpro.c
@@ -40,15 +40,6 @@
 #define MPRO_MAX_DELAY		100
 #define MPRO_INPUT_TRS_SIZE	14
 
-#define MODEL_DEFAULT		"MPRO\n"
-#define MODEL_5IN		"MPRO-5\n"
-#define MODEL_5IN_OLED		"MPRO-5H\n"
-#define MODEL_4IN3		"MPRO-4IN3\n"
-#define MODEL_4IN		"MPRO-4\n"
-#define MODEL_6IN8		"MPRO-6IN8\n"
-#define MODEL_3IN4		"MPRO-3IN4\n"
-#define MODEL_10IN		"MPRO-10\n"
-
 struct mpro_device {
 	struct drm_device dev;
 	struct device *dmadev;
@@ -65,8 +56,6 @@ struct mpro_device {
 	unsigned int width;
 	unsigned int height;
 	unsigned int margin;
-	unsigned int width_mm;
-	unsigned int height_mm;
 
 	unsigned char cmd[64];
 	unsigned char *draw_buf;
@@ -123,10 +112,6 @@ static const char cmd_get_version[5] = {
 static const char cmd_get_id[5] = {
 	0x51, 0x02, 0x08, 0x1f, 0xf0
 };
-
-//static char cmd_draw[6] = {
-//	0x00, 0x2c, 0x00, 0x00, 0x00, 0x00
-//};
 
 static char cmd_draw_part[12] = {
 	0x00, 0x2c, 0x00, 0x00, 0x00, 0x00,
@@ -462,37 +447,23 @@ static const struct drm_mode_config_funcs mpro_mode_config_funcs = {
 	.atomic_commit = drm_atomic_helper_commit,
 };
 
-static int mpro_get_screen(struct mpro_device *mpro)
+static void mpro_mode_config_setup(struct mpro_device *mpro, unsigned char *v)
 {
-	struct usb_device *udev = mpro_to_usb_device(mpro);
-	void *cmd = mpro->cmd;
-	int ret;
+	struct drm_device *dev = &mpro->dev;
+	int version_major, version_minor;
+	int width, height;
 
-	ret = usb_control_msg(udev, usb_sndctrlpipe(udev, 0),
-			      0xb5, 0x40, 0, 0,
-			      (void *)cmd_get_screen, 5,
-			      MPRO_MAX_DELAY);
-	if (ret < 5)
-		goto err;
+	sscanf(v, "v%d.%d, %dx%d", &version_major, &version_minor,
+			&width, &height);
 
-	ret = usb_control_msg(udev, usb_rcvctrlpipe(udev, 0),
-			      0xb6, 0xc0, 0, 0,
-			      cmd, 1,
-			      MPRO_MAX_DELAY);
-	if (ret < 1)
-		goto err;
+	mpro->width = width;
+	mpro->height = height;
 
-	ret = usb_control_msg(udev, usb_rcvctrlpipe(udev, 0),
-			      0xb7, 0xc0, 0, 0,
-			      cmd, 5,
-			      MPRO_MAX_DELAY);
-	if (ret < 5)
-		goto err;
-
-	mpro->screen = ((unsigned int *)(cmd + 1))[0];
-	return 0;
-err:
-	return -EIO;
+	dev->mode_config.funcs = &mpro_mode_config_funcs;
+	dev->mode_config.min_width = width;
+	dev->mode_config.max_width = width;
+	dev->mode_config.min_height = height;
+	dev->mode_config.max_height = height;
 }
 
 static int mpro_get_version(struct mpro_device *mpro)
@@ -501,28 +472,15 @@ static int mpro_get_version(struct mpro_device *mpro)
 	void *cmd = mpro->cmd;
 	int ret;
 
-	ret = usb_control_msg(udev, usb_sndctrlpipe(udev, 0),
-			      0xb5, 0x40, 0, 0,
-			      (void *)cmd_get_version, 5,
-			      MPRO_MAX_DELAY);
-	if (ret < 5)
-		goto err;
-
 	ret = usb_control_msg(udev, usb_rcvctrlpipe(udev, 0),
-			      0xb6, 0xc0, 0, 0,
-			      cmd, 1,
+			      0xa0, 0xc0, 0, 0,
+			      cmd, 64,
 			      MPRO_MAX_DELAY);
-	if (ret < 1)
+	if (ret < 0)
 		goto err;
 
-	ret = usb_control_msg(udev, usb_rcvctrlpipe(udev, 0),
-			      0xb7, 0xc0, 0, 0,
-			      cmd, 5,
-			      MPRO_MAX_DELAY);
-	if (ret < 5)
-		goto err;
+	mpro_mode_config_setup(mpro, cmd);
 
-	mpro->version = ((unsigned int *)(cmd + 1))[0];
 	return 0;
 err:
 	return -EIO;
@@ -562,88 +520,6 @@ err:
 	return -EIO;
 }
 
-static void mpro_mode_config_setup(struct mpro_device *mpro)
-{
-	struct drm_device *dev = &mpro->dev;
-	int width, height, margin, width_mm, height_mm;
-	char *model = MODEL_DEFAULT;
-
-	width  = 480;
-	height = 800;
-	margin = 0;
-	width_mm = 0;
-	height_mm = 0;
-
-	switch (mpro->screen) {
-	case 0x00000005:
-		model = MODEL_5IN;
-		height = 854;
-		width_mm = 62;
-		height_mm = 110;
-		if (mpro->version != 0x00000003)
-			margin = 320;
-		break;
-
-	case 0x00001005:
-		model = MODEL_5IN_OLED;
-		width = 720;
-		height = 1280;
-		width_mm = 62;
-		height_mm = 110;
-		break;
-
-	case 0x00000304:
-		model = MODEL_4IN3;
-		width_mm = 56;
-		height_mm = 94;
-		break;
-
-	case 0x00000004:
-	case 0x00000b04:
-	case 0x00000104:
-		model = MODEL_4IN;
-		width_mm = 53;
-		height_mm = 86;
-		break;
-
-	case 0x00000007:
-		model = MODEL_6IN8;
-		width = 800;
-		height = 480;
-		width_mm = 89;
-		height_mm = 148;
-		break;
-
-	case 0x00000403:
-		model = MODEL_3IN4;
-		width = 800;
-		height = 800;
-		width_mm = 88;
-		height_mm = 88;
-		break;
-
-	case 0x0000000a:
-		model = MODEL_10IN;
-		width = 1024;
-		height = 600;
-		width_mm = 222;
-		height_mm = 125;
-	}
-
-	mpro->width = width;
-	mpro->height = height;
-	mpro->margin = margin;
-	mpro->model = model;
-	mpro->width_mm = width_mm;
-	mpro->height_mm = height_mm;
-
-	dev->mode_config.funcs = &mpro_mode_config_funcs;
-	dev->mode_config.min_width = width;
-	dev->mode_config.max_width = width;
-	dev->mode_config.min_height = height;
-	dev->mode_config.max_height = height;
-}
-
 static int mpro_edid_block_checksum(u8 *raw_edid)
 {
 	int i;
@@ -659,13 +535,11 @@ static int mpro_edid_block_checksum(u8 *raw_edid)
 
 static void mpro_edid_setup(struct mpro_device *mpro)
 {
-	unsigned int width, height, width_mm, height_mm;
+	unsigned int width, height;
 	char buf[16];
 
 	width = mpro->width;
 	height = mpro->height;
-	width_mm = mpro->width_mm;
-	height_mm = mpro->height_mm;
 
 	mpro_edid.detailed_timings[0].data.pixel_data.hactive_lo = width % 256;
 	mpro_edid.detailed_timings[0].data.pixel_data.hactive_hblank_hi &= 0x0f;
@@ -676,17 +550,6 @@ static void mpro_edid_setup(struct mpro_device *mpro)
 	mpro_edid.detailed_timings[0].data.pixel_data.vactive_vblank_hi &= 0x0f;
 	mpro_edid.detailed_timings[0].data.pixel_data.vactive_vblank_hi |= \
 						((u8)(height / 256) << 4);
-
-	mpro_edid.detailed_timings[0].data.pixel_data.width_mm_lo = \
-							width_mm % 256;
-	mpro_edid.detailed_timings[0].data.pixel_data.height_mm_lo = \
-							height_mm % 256;
-	mpro_edid.detailed_timings[0].data.pixel_data.width_height_mm_hi = \
-					((u8)(width_mm / 256) << 4) | \
-					((u8)(height_mm / 256) & 0xf);
-
-	memcpy(&mpro_edid.detailed_timings[2].data.other_data.data.str.str,
-		mpro->model, strlen(mpro->model));
 
 	snprintf(buf, 16, "%02X%02X%02X%02X\n",
 		mpro->id[4], mpro->id[5], mpro->id[6], mpro->id[7]);
@@ -821,12 +684,6 @@ static int mpro_usb_probe(struct usb_interface *interface,
 	if (!mpro->dmadev)
 		drm_warn(dev, "buffer sharing not supported"); /* not an error */
 
-	ret = mpro_get_screen(mpro);
-	if (ret) {
-		drm_err(dev, "cant't get screen info.\n");
-		goto err_put_device;
-	}
-
 	ret = mpro_get_version(mpro);
 	if (ret) {
 		drm_err(dev, "cant't get screen version.\n");
@@ -843,7 +700,6 @@ static int mpro_usb_probe(struct usb_interface *interface,
 	if (ret)
 		goto err_put_device;
 
-	mpro_mode_config_setup(mpro);
 	mpro_edid_setup(mpro);
 
 	ret = mpro_data_alloc(mpro);
